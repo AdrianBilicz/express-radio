f9dbb1cf8261223db74c5f934c132305fb252714
Stephen Mayeux <stephenmayeux@gmail.com>
2017-07-30 09:01:24 -0500

    today's lesson

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
-
-
-
+var mongoose = require('mongoose')
 
 var index = require('./routes/index');
-
-
 var app = express();
 
-
+mongoose.connect('mongodb://adrian:qwerty@ds125113.mlab.com:25113/express_radio')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+// Or use the below option
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use('/img',express.static(path.join(__dirname, 'public/images')));
 app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
 app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
 app.use('/', index);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,158 +2,109 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var mongoose = require('mongoose');
+var async = require('async')
 
-
-//object sended with every response
-album_info = {
-	album_photo: '#',
-	title: '',
-	uri: '#',
-	resource_url: '#',
-	song_info: false,
-	empty_area: false,
-	video_info: {
-		is_video: false,
-		video_url: '#',
-		fb_link: '#',
-		lowest_price: 0
-	}
-}
+var Url = require('../models/url')
 
 //prevent requesting for favicon
 router.get('/favicon.ico', function(req, res) {
-	res.status(204);
+  res.status(204);
 });
-
-// Database connection
-var Url = require('../models/url')
-mongoose.connect('mongodb://adrian:qwerty@ds125113.mlab.com:25113/express_radio')
 
 //Uploading youtube video to database
 router.post('/upload', function(req, res, next) {
-	var title = req.body.album_info.title
-	var youtube_link = req.body.youtube_link
-	//create new database record
-	var new_record = new Url({
-		title: title,
-		video_url: youtube_link
-	})
-	//save database record
-	new_record.save(function(err) {
-		if (err) { 
-			return res.send({ success: false, msg: 'error writing to database' })
-		}
-		console.log('ok')
-	})
-
-
+	const { album_info: { title }, youtube_link } = req.body
+  //create new database record
+  var new_record = new Url({ title, video_url: youtube_link })
+  //save database record
+  new_record.save(function(err) {
+    if (err) return res.send({ success: false, msg: 'error writing to database' })
+    console.log('ok')
+  })
 });
-
-// router.post('/albums', function(req, res, next) {
-// 	var setup = req.body,
-// 	token = 'PrwFREBcgmJxCDHzelVWgSObSBOxvvgtGPXiBFeI',
-// 	api_search = 'https://api.discogs.com/database/search?'
-
-// 	request(`${api_search}genre=${setup.genre}&country=${setup.country}&year=${setup.year}&token=${token}`, options , function (error, response, body) {
-// 		console.log(response.statusCode)
-// 		if (!error && response.statusCode == 200) {
-
-// 			var albums_collection = JSON.parse(response.body)
-
-// 			var how_many_albums = String(albums_collection.pagination.items)
-// 			res.send(how_many_albums)
-// 		}
-// 	})
-
-// });
-
 
 /* GET home page. */
 router.get('/radioo', function(req, res, next) {
-
-	res.render('index',album_info);
+  res.render('index', { album_info: { song_info: false } });
 });
-
-//api request options
-var options = {
-	headers: {'user-agent': 'node.js'}
-}
 
 //route for requesting data from externall api
 router.post('/radioo', function(req, res, next) {
-	var setup = req.body,
-	token = 'PrwFREBcgmJxCDHzelVWgSObSBOxvvgtGPXiBFeI',
-	api_search = 'https://api.discogs.com/database/search?'
+	//api request options
+	var options = { headers: { 'user-agent': 'node.js' } }
+  var setup = req.body,
+    	token = 'PrwFREBcgmJxCDHzelVWgSObSBOxvvgtGPXiBFeI',
+    	api_search = 'https://api.discogs.com/database/search?'
 
+	async.waterfall([
+		function(callback) {
+			request(`${api_search}genre=${setup.genre}&country=${setup.country}&year=${setup.year}&token=${token}`, options, function(error, response, body) {
+				console.log(response.statusCode)
+				if (error || response.statusCode >= 400) return callback(error)
 
+				//grab collection from api
+				var albums_collection = JSON.parse(response.body)
+				//assigning album data to variables
+				var item = Math.floor(Math.random() * albums_collection.results.length);
+				var thumb = albums_collection.results.length !== 0 ? albums_collection.results[item].thumb : '/img/nopreview.jpeg'
+				var title = albums_collection.results.length !== 0 ? albums_collection.results[item].title : 'This area/decade is empty at this time'
+				var uri = albums_collection.results.length !== 0 ? `https://www.discogs.com/${albums_collection.results[item].uri}` : '#'
+				var resource_url = albums_collection.results.length !== 0 ? albums_collection.results[item].resource_url : null
 
-	request(`${api_search}genre=${setup.genre}&country=${setup.country}&year=${setup.year}&token=${token}`, options , function (error, response, body) {
-		console.log(response.statusCode)
-		if (!error && response.statusCode == 200) {
-			//grab collection from api
-			var albums_collection = JSON.parse(response.body)
-			//assigning album data to variables
-			var item = Math.floor(Math.random()*albums_collection.results.length);
-			var thumb = albums_collection.results.length !== 0 ? albums_collection.results[item].thumb : '/img/nopreview.jpeg'
-			var title = albums_collection.results.length !== 0 ? albums_collection.results[item].title : 'This area/decade is empty at this time'
-			var uri = albums_collection.results.length !== 0 ? `https://www.discogs.com/${albums_collection.results[item].uri}` : '#'
-			var resource_url = albums_collection.results.length !== 0 ? albums_collection.results[item].resource_url : null
-
-			album_info = {
-				album_photo: thumb,
-				title: title,
-				uri: uri,
-				resource_url: resource_url,
-				song_info: true,
-				empty_area: false,
-				video_info: {}
+				var album_info = {
+					album_photo: thumb,
+					title: title,
+					uri: uri,
+					resource_url: resource_url,
+					song_info: true,
+					empty_area: false,
+					video_info: {}
+				}
+				callback(album_info)
 			}
-
+		},
+		function(album_info, callback) {
 			//if there are any matches
-			if(album_info.resource_url){
+			if (album_info.resource_url) {
+
 				//make another call for specific album
-				request(`${resource_url}`, options , function (error, response, body) {
-					if (!error && response.statusCode == 200) {
-						//grabing data 
-						var random_album_data = JSON.parse(response.body)
-						var lowest_price = random_album_data.lowest_price ? `${Math.round(random_album_data.lowest_price)}$` : '$'
+				request(`${resource_url}`, options, function(error, response, body) {
+					//grabing data
+					var random_album_data = JSON.parse(response.body)
+					var lowest_price = random_album_data.lowest_price ? `${Math.round(random_album_data.lowest_price)}$` : '$'
 
-						//checking first if there is a video in my database
-						Url.findOne({ title: title },function(err, result){
-							if(err){
-								return res.send({ success: false, msg: 'Error reaading from db', error: err })
+					//checking first if there is a video in my database
+					Url.findOne({ title: title }, function(err, result) {
+						if (err) callback(err)
+
+						//if there is no video in database assign discogs video
+						if (result || random_album_data.videos) {
+							var ytb_url = result ? result.video_url : random_album_data.videos[0].uri
+							var iframe_url = ytb_url.replace('watch?v=', 'embed/') + '?autoplay=1';
+							var fb_url = `https://www.facebook.com/sharer/sharer.php?u=${ytb_url}`;
+							album_info.video_info = {
+								is_video: true,
+								video_url: iframe_url,
+								fb_link: fb_url,
+								lowest_price: lowest_price
 							}
-							//if there is no video in database assign discogs video
-							if(result  || random_album_data.videos){
-								var ytb_url = result ? result.video_url : random_album_data.videos[0].uri
-								var iframe_url = ytb_url.replace('watch?v=','embed/') + '?autoplay=1';
-								var fb_url = `https://www.facebook.com/sharer/sharer.php?u=${ytb_url}`;
-								album_info.video_info ={
-									is_video: true,
-									video_url: iframe_url,
-									fb_link: fb_url,
-									lowest_price: lowest_price
-								}
-								return res.send(albums_collection)
-							//if there is no video in both ddatabases 
-							}else{
-								album_info.video_info ={
-									is_video: false,
-									video_url: '#',
-									fb_link: '#',
-									lowest_price: lowest_price
-								}
-								return res.send(album_info)
+							return callback(album_info)
+							//if there is no video in both ddatabases
+						} else {
+							album_info.video_info = {
+								is_video: false,
+								video_url: '#',
+								fb_link: '#',
+								lowest_price: lowest_price
 							}
-
-
-						})
-						
-					}
+							callback(album_info)
+						}
+					})
 				})
+			}
 			//if there aren't any matches
-			}else{
-				album_info = {
+			else {
+				var album_info = {
 					album_photo: thumb,
 					title: title,
 					uri: uri,
@@ -164,12 +115,13 @@ router.post('/radioo', function(req, res, next) {
 						is_video: false
 					}
 				}
-				
-				return res.send(album_info)
-
+				return callback(album_info)
 			}
 		}
+	], function(err, album_info) {
+		if (err) return res.send({ error: err, msg: 'There was an error' })
+		res.render('index', { album_info })
 	})
 });
-module.exports = router;
 
+module.exports = router;
